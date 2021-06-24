24ce83fc357862ab8f89594ea381f4ce947899d1
Huey Kim <hwuiwon.kim@gmail.com>
2021-06-24 17:52:34 +0900

    Edited query for better performance

--- src/service/stockInfoService.js
+++ src/service/stockInfoService.js
@@ -13,11 +13,12 @@ let stockObj;
 const pUrl = 'https://fchart.stock.naver.com/sise.nhn?timeframe=day&requestType=0&symbol=';
 
 /**
- * 종목을 클릭했을 때 나오는 상세 항목에 들어가는 정보를 리턴
+ * 종목을 클릭했을 때 나오는 상세 항목에 들어가는 정보를 밑의 순서대로 리턴
  *
- * 종목 이름, 종목 정보, 시가총액, 52주 최고/최저가, 외국인 소진률
- * PER, PBR, ROE, 현재가, 변동률, 기대 수익률, 컨센서스 평균가
- * 최근 30일 주가, 키워드
+ * 종목이름, 종목정보, 현재가, 변동률, 시가총액
+ * 52주 최고/최저가, 외국인소진률, PER, PBR, ROE
+ * 애널리스트 평균가, 상승여력, 추천여부, 일별 가격 (1달)
+ * 키워드, 리포트 리스트, 매매동향
  */
 
 /**
@@ -50,6 +51,7 @@ async function getReports(stockId, date) {
     const query = {
         TableName: 'reportListComplete',
         IndexName: "stockId-date-index",
+        ProjectionExpression: '#dt, reportName, analyst, priceGoal, firm, reportIdx',
         KeyConditionExpression: '#id = :id and #dt >= :date',
         ExpressionAttributeNames: {
             '#id': 'stockId',
@@ -108,8 +110,8 @@ async function getKeyword(reportList) {
     // 띄어쓰기 없어도 잘 추출하니까 reportList 보낼 때 안에 있는 리포트 제목들의 공백 전부 제거하고 보내는게 나을듯
 
     const reportTitles = [];
-    for (const report of reportList) {
-        reportTitles.push(report.reportName.replace(/ /g,''));
+    for (let i=0; i<reportList.length; i++) {
+        reportTitles.push(reportList[i].reportName.replace(/ /g,''));
     }
     return reportTitles;
 }
@@ -159,14 +161,14 @@ async function getInvestor(stockId) {
 
     try {
         body = await axios.get(generateUrl(kUrl, params));
-        for (const info of body.data.output) {
+        body.data.output.forEach(info => {
             investInfo.push({
                 date: info.TRD_DD,
                 individual: numToKR(info.TRDVAL3),
                 foreign: numToKR(info.TRDVAL4),
                 institutions: numToKR(info.TRDVAL1)
             })
-        }
+        })
     } catch (error) { console.log('[stockInfoService]: Error in getInvestor'); }
 
     return investInfo;
@@ -192,7 +194,7 @@ function numToKR(number) {
     if (isNegative) {
         return '-' + num;
     } else {
-        return num;
+        return '+' + num;
     }
 }
 
@@ -203,7 +205,6 @@ function numToKR(number) {
  * @param date Lookup start date (YYYY-MM-DD)
  */
 async function getStockOverview(stockId, date) {
-    let reportList = [];
     stockObj = {};
 
     const reports = await getReports(stockId, date);
@@ -227,18 +228,7 @@ async function getStockOverview(stockId, date) {
     stockObj['expYield'] > 0 ? stockObj['recommend'] = 'O' : stockObj['recommend'] = 'X';
     stockObj['past30Price'] = past30Price;
     stockObj['keywords'] = keywords;
-
-    for (const report of reports) {
-        reportList.push({
-            date: report.date,
-            reportName: report.reportName,
-            analyst: report.analyst,
-            priceGoal: report.priceGoal,
-            firm: report.firm,
-            reportIdx: report.reportIdx
-        })
-    }
-    stockObj['reportList'] = reportList;
+    stockObj['reportList'] = reports;
     stockObj['invStatistics'] = invStatistics;
 
     return stockObj;
