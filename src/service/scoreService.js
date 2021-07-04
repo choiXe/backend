435373a22bb4691ede62f4cb8495b9639c3f66b3
Huey Kim <hwuiwon.kim@gmail.com>
2021-07-04 16:30:40 +0900

    Making scoreService.js into a independent scheduler service

--- src/service/scoreService.js
+++ src/service/scoreService.js
@@ -5,12 +5,17 @@
  *
  * Categories
  *
- * 인기도 (Popularity)
+ * 신뢰도 (Credibility)
  *     공통 (성장주 & 가치주)
+ *         애널리스트 정확도: 높을수록 점수 ↑
  *         expYield: 애널리스트 평균가와 현재 가격의 괴리율 (상승 여력이 높을수록 점수 ↑)
  *         consensusCount: 발행된 리포트의 개수 (많을수록 점수 ↑)
+ *
+ * 인기도 & 추세 (Popularity)
+ *     공통 (성장주 & 가치주)
  *         volume: 거래량 (증가할수록 점수 ↑)
  *         communityIndex: 커뮤니티 활성도 (증가할수록 점수 ↑)
+ *         momentum: 모멘텀 수치 (높을수록 점수 ↑)
  *
  * 재무 (Financials)
  *     성장주
@@ -31,16 +36,47 @@
  *     최근 3달 내에 리포트가 하나 이상 발행된 종목일 것
  *
  * 점수 업데이트 방법:
- *     1. 유저가 종목 상세정보를 클릭할 때 DB 에서 쿼리로 불러오고 오늘 업데이트 된 기록이 있을때 그대로 사용
- *        만약 DB 에 있는 데이터가 최신(오늘)의 데이터가 아닐 경우 정보를 파싱해서 유저에게 보여줌과 동시에 DB 에 저장
- *     2. 특정 시간 (새벽)에 작동하는 스케쥴러를 만들어서 자동으로 계산
- *        DynamoDB 상에서 이렇게 하려면 Query 가 아닌 Scan 을 써야 하는데 너무 비효율적임
- *        만약 query 로 가져올 수 있는 방법이 있다면 가장 나은 방법임
- *     3. Any suggestions?
+ *     특정 시간 (새벽)에 작동하는 스케쥴러를 만들어서 자동으로 계산
  */
 
+const AWS = require('aws-sdk');
+const axios = require('axios');
+
+const {region, timeoutLimit} = require('../data/constants.js');
+const {lSectors} = require('../data/wicsDictionary.js');
+const {scoreQuery} = require('../data/queries.js');
+
+AWS.config.update(region);
+const docClient = new AWS.DynamoDB.DocumentClient();
+axios.defaults.timeout = timeoutLimit;
+
 /**
- * Returns recommendation score of a stock
+ * Returns list of stockIds and their report count & priceGoal
+ * that has at least 1 report for past 3 months
+ */
+async function getIdList() {
+    let reportList = [], stockList = {};
+    let price;
+
+    for (const sector of lSectors) {
+        reportList.push.apply(reportList,
+            (await docClient.query(scoreQuery(sector)).promise()).Items);
+    }
+    reportList.forEach(item => {
+        price = parseInt(item.priceGoal);
+        if (!stockList[item.stockId]) {
+            stockList[item.stockId] = { count: 1 }
+            price !== 0 ? stockList[item.stockId].price = [price] :
+                stockList[item.stockId].price = [];
+        }
+        stockList[item.stockId].count++;
+        if (price !== 0) stockList[item.stockId].price.push(price);
+    })
+    return stockList;
+}
+
+/**
+ * Returns score recommendation of a stock
  * @param expYield expected profit yield
  * @param consensusCount number of reports backing up priceGoal
  */
@@ -63,5 +99,12 @@ function getScore(expYield, consensusCount) {
     }
     return Math.sign(expYield) * Math.round(a + b) * 5;
 }
+
+async function test() {
+    const a = await getIdList();
+    console.log(a);
+}
+
+test();
 
 module.exports = {getScore};
